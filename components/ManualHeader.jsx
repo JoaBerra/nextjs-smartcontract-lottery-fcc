4d27b0021a9b682467f0ebcb4036ba5c8d463593
JoaBerra <31992783+JoaBerra@users.noreply.github.com>
2023-02-10 07:04:18 +0100

    initial commit

---
+++ components/ManualHeader.jsx
@@ -0,0 +1,55 @@
+import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
+
+export default function ManualHeader() {
+    const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3, isWeb3EnableLoading } =
+        useMoralis()
+
+    useEffect(() => {
+        if (
+            !isWeb3Enabled &&
+            typeof window !== "undefined" &&
+            window.localStorage.getItem("connected")
+        ) {
+            enableWeb3()
+            // enableWeb3({provider: window.localStorage.getItem("connected")}) // add walletconnect
+        }
+    }, [isWeb3Enabled])
+    // no array, run on every render
+    // empty array, run once
+    // dependency array, run when the stuff in it changesan
+
+    useEffect(() => {
+        Moralis.onAccountChanged((account) => {
+            console.log(`Account changed to ${account}`)
+            if (account == null) {
+                window.localStorage.removeItem("connected")
+                deactivateWeb3()
+                console.log("Null account found")
+            }
+        })
+    }, [])
+
+    return (
+        <div>
+            {account ? (
+                <div>
+                    Connected to {account.slice(0, 6)}...
+                    {account.slice(account.length - 4, account.length)}
+                </div>
+            ) : (
+                <button
+                    onClick={async () => {
+                        await enableWeb3()
+                        if (typeof window !== "undefined") {
+                            window.localStorage.setItem("connected", "injected")
+                        }
+                    }}
+                    disabled={isWeb3EnableLoading}
+                >
+                    Connect
+                </button>
+            )}
+        </div>
+    )
+}
